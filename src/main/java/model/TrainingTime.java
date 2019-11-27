cac9d525fb948f106609ec2626933fa4195725a1
rodedrengen <simon.588.jorgensen@gmail.com>
2019-11-27 15:59:25 +0100

    TrainningTimeMapper

--- src/main/java/model/TrainingTime.java
+++ src/main/java/model/TrainingTime.java
@@ -1,10 +1,28 @@
 
 package model;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author <Frederik Keis Dinsen>
  */
 public class TrainingTime extends Times {
+    private int memeberID;
+    private LocalDate date;
+    
+    public TrainingTime(int memberID, LocalDate date, int timeInMs, SwimmingDiscipline sd){
+        super(timeInMs,sd);
+        this.memeberID = memberID;
+        this.date = date;
+        
+    }
 
+    public int getMemeberID() {
+        return memeberID;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
 }
