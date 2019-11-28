ecc3617fc409de75bb33e666b5227e1f44ab5dba
Oliver Vang <olv.vang@gmail.com>
2019-11-28 10:14:02 +0100

    Tournament constructor and getters added.

--- src/main/java/model/Tournament.java
+++ src/main/java/model/Tournament.java
@@ -1,10 +1,35 @@
 
 package model;
 
-/**
- *
- * @author <Frederik Keis Dinsen>
- */
-public class Tournament {
+import java.time.LocalDate;
 
+
+public class Tournament {
+    private int tId;
+    private String tName;
+    private LocalDate tDate;
+    private String Location;
+
+    public Tournament(int tId, String tName, LocalDate tDate, String tLocation) {
+        this.tId = tId;
+        this.tName = tName;
+        this.tDate = tDate;
+        this.Location = tLocation;
+    }
+
+    public int gettId() {
+        return tId;
+    }
+
+    public String gettName() {
+        return tName;
+    }
+
+    public LocalDate gettDate() {
+        return tDate;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
 }
