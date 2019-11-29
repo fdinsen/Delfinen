fbd0b073ddb15c3f3a9b8ee3a3fcd267be576561
Frederik Dinsen <gammarik@gmail.com>
2019-11-29 09:44:59 +0100

    Started work on TrainerMapper, changed regex for phone in UI and changed trainer_id_name to trainer_name in database

--- src/main/java/datasource/TrainerMapper.java
+++ src/main/java/datasource/TrainerMapper.java
@@ -1,10 +1,51 @@
-
 package datasource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Trainer;
 
 /**
  *
  * @author <Frederik Keis Dinsen>
  */
 public class TrainerMapper {
-    
+
+    private Connection con = null;
+
+    public void createTrainer(Trainer trainer) {
+        con = DBConnector.getConnection();
+        String sql = "INSERT INTO trainers (trainer_name) VALUES (?)";
+        try (PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, trainer.getTrainerName());
+            ps.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(RestanceMapper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void updateTrainer(Trainer trainer) {
+        con = DBConnector.getConnection();
+        String newTrainerName = trainer.getTrainerName();
+        int oldTrainerId = trainer.getId();
+        String sql = "UPDATE trainers SET trainer_name = ? WHERE trainer_id = ?";
+        try(PreparedStatement ps = con.prepareStatement(sql)) {
+            ps.setString(1, newTrainerName);
+            ps.setInt(2, oldTrainerId);
+            ps.execute();
+        } catch (SQLException ex) {
+                Logger.getLogger(RestanceMapper.class.getName()).log(Level.SEVERE, null, ex);
+            }
+    }
+
+    public void deleteTrainer(int trainerId) {
+
+    }
+
+    public Trainer getTrainer(int trainerId) {
+
+        return new Trainer("temp");
+    }
 }
--- src/main/java/model/Trainer.java
+++ src/main/java/model/Trainer.java
@@ -6,5 +6,30 @@ package model;
  * @author <Frederik Keis Dinsen>
  */
 public class Trainer {
-
+    //--------------------//
+    // INSTANCE VARIABLES //
+    //--------------------//
+    String name;
+    int id;
+    
+    //-------------//
+    // CONSTRUCTOR //
+    //-------------//
+    public Trainer(String trainerName) {
+        name = trainerName;
+    }
+    
+    public Trainer(String trainerName, int id)  {
+        name = trainerName;
+        this.id = id;
+    }
+    //---------//
+    // GETTERS //
+    //---------//
+    public String getTrainerName() {
+        return name;
+    }
+    public int getId() {
+        return id;
+    }
 }
