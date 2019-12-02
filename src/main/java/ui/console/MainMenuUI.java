9f5d6746851cfd63216b7ded7b31a6b720686ab8
olvang <olv.vang@gmail.com>
2019-12-02 15:13:01 +0100

    Removed _

--- src/main/java/ui/console/MainMenuUI.java
+++ src/main/java/ui/console/MainMenuUI.java
@@ -8,8 +8,46 @@ import Controllers.Controller;
  * @author <Frederik Keis Dinsen>
  */
 public class MainMenuUI extends UI {
+    private Controller controller;
+    protected MainMenuUI(Controller controller) {
+        this.controller = controller;
+    }
     
-    protected MainMenuUI() {
- 
+    public void printMainMenu(){
+        Boolean exit = false;
+        int counter = 0;
+        String StringMenu = "";
+        do {
+            if(controller.getUserPrivileges()[2]){
+                counter++;
+                StringMenu += counter + ". Medlemmer\n";
+            }
+            
+            if(controller.getUserPrivileges()[6]){
+                counter++;
+                StringMenu += counter + ". Restance\n";
+            }
+            
+            if(controller.getUserPrivileges()[9]){
+                counter++;
+                StringMenu += counter + ". Stævner\n";
+            }
+            
+            if(controller.getUserPrivileges()[14]){
+                counter++;
+                StringMenu += counter + ". Top 5 tider\n";
+            }
+            
+            if(controller.getUserPrivileges()[4] || controller.getUserPrivileges()[5]){
+                counter++;
+                StringMenu += counter + ". Tilføj/Ret Træner\n";
+            }
+            printExit();
+        } while (!exit);
+        
+    }
+    
+    private void printExit(){
+        print("0. Exit");
     }
 }
