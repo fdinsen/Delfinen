2eadf283670ec485e9939bd8072de539b1fb0c88
Frederik Dinsen <gammarik@gmail.com>
2019-12-05 17:25:31 +0100

    added admin sql script

---
+++ SQLScripts/delfinenTilføjAdmin.sql
@@ -0,0 +1,5 @@
+SELECT * FROM user_privileges;
+
+INSERT INTO user_privileges  
+(create_member, see_member,edit_member,create_trainer,edit_trainer,see_restance, mark_as_payed, add_training_time,see_tournament,add_tournament, see_competition,add_competition,add_member_to_competition,show_top_5,edit_competition,edit_tournament) 
+VALUES (1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
